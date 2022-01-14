fb4f31fe91b25c1534c13f2a75b135686cdaa9df
syi07030 <syii07030@gmail.com>
2022-01-14 23:47:49 +0900

    2022.1.14[1]

--- 체육복[level1] - greedy/mycode.py
+++ 체육복[level1] - greedy/mycode.py
@@ -14,4 +14,21 @@ def solution(n, lost, reserve):
     answer = n-len(lost)
     return answer
 
-#2
+#2 - 3문제 실패, lost에 있는 거랑 reserve에 있는거랑 만약 같은 값이 뒤쪽에 있으면 그 값은 원래 자기거만 사용가능한데 오류,,
+def solution(n, lost, reserve):
+    answer = 0
+    lost_reserve=[]
+    for i in lost:
+        if i in reserve:
+            lost_reserve.append(i)
+            reserve.remove(i)
+        elif i-1 in reserve:
+            lost_reserve.append(i)
+            reserve.remove(i-1)
+        elif i+1 in reserve:
+            lost_reserve.append(i)
+            reserve.remove(i+1)
+    answer = n-(len(lost)-len(lost_reserve))
+    return answer
+
+#3
