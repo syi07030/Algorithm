c782e01f6c4141f48acbcfbfdb6ea926fcc52354
syi07030 <syii07030@gmail.com>
2022-01-19 22:27:42 +0900

    2022.1.19[1]

---
+++ 짝수와 홀수[level1]/mycode.swift
@@ -0,0 +1,12 @@
+func solution(_ num:Int) -> String {
+    if num%2==0{
+        return "Even"
+    }
+    else{
+        return "Odd"
+    }
+}
+
+//func solution(_ num:Int) -> String {
+//    return num % 2 == 0 ? "Even" : "Odd"
+//}
