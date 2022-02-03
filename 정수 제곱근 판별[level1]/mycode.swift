311e6eb3c8da667b15756ffb8d26db8d39a0aca8
syi07030 <syii07030@gmail.com>
2022-02-03 21:32:35 +0900

    2022.2.3[3]

--- 정수 제곱근 판별[level1]/mycode.swift
+++ 정수 제곱근 판별[level1]/mycode.swift
@@ -1,9 +1,23 @@
+//1차
 import Foundation
 func solution(_ n:Int64) -> Int64 {
     let num: Double = Double(n)
     //let sqrt: Double = sqrt(num)
     if(sqrt(num) == round(sqrt(num))){
         return Int64(sqrt(num)+1)*Int64(sqrt(num)+1)
+    }
+    else{
+        return -1
+    }
+}
+
+//2차
+import Foundation
+func solution(_ n:Int64) -> Int64 {
+    let num: Double = Double(n)
+    let s = Int64(sqrt(num))
+    if(Double(s) == sqrt(num)){
+        return (s+1)*(s+1)
     }
     else{
         return -1
