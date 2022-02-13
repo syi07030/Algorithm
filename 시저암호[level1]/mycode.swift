65caebf4ed24658b326695c3c26033d0bd2d424a
syi07030 <syii07030@gmail.com>
2022-02-13 23:54:21 +0900

    2022.2.13[2]

--- 시저암호[level1]/mycode.swift
+++ 시저암호[level1]/mycode.swift
@@ -19,3 +19,33 @@ func solution(_ s:String, _ n:Int) -> String {
     }
     return answer
 }
+
+/*
+다른 사람 풀이 코드
+
+func solution(_ s:String, _ n:Int) -> String {
+    return s.utf8.map {
+        var code = Int($0)
+        switch code {
+            case 65...90:
+                code = (code + n - 65) % 26 + 65
+            case 97...122:
+                code = (code + n - 97) % 26 + 97
+            default:
+                break
+        }
+        return String(UnicodeScalar(code)!)
+    }.joined()
+}
+
+
+func solution(_ s:String, _ n:Int) -> String {
+    let alphabets = "abcdefghijklmnopqrstuvwxyz".map { $0 }
+    return String(s.map {
+        guard let index = alphabets.firstIndex(of: Character($0.lowercased())) else { return $0 }
+        let letter = alphabets[(index + n) % alphabets.count]
+        return $0.isUppercase ? Character(letter.uppercased()) : letter
+    })
+}
+
+*/
