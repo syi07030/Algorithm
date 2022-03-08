ca01e943ed43c24ab7d1877feb2c44e3003dc5d2
syi07030 <syii07030@gmail.com>
2022-03-08 23:36:03 +0900

    2022.3.8[1]

---
+++ 나누어 떨어지는 숫자 배열[level1]/mycode.swift
@@ -0,0 +1,15 @@
+func solution(_ arr:[Int], _ divisor:Int) -> [Int] {
+    var answer = [Int]()
+    
+    for i in arr {
+        if i%divisor == 0 {
+            answer.append(i)
+        }
+    }
+    
+    if answer.count == 0 {
+        answer.append(-1)
+    }
+    
+    return answer.sorted()
+}
