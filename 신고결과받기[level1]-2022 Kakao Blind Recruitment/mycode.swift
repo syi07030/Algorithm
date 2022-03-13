626d138ace912fc9aa938bbce1358c0cdd6b33fa
syi07030 <syii07030@gmail.com>
2022-03-13 16:03:35 +0900

    2022.3.13[2]

--- 신고결과받기[level1]-2022 Kakao Blind Recruitment/mycode.swift
+++ 신고결과받기[level1]-2022 Kakao Blind Recruitment/mycode.swift
@@ -6,32 +6,19 @@ func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
     let report_set = Set(report)
     let report_arr = Array(report_set) 
     //중복 제거 -> 한 유저가 같은 유저를 여러 번 신고한 경우 신고 횟수는 1번
-    
-    var reportUser = [String]() //신고한 유저
-    var reportedUser = [String]() //신고당한 유저
-    
-    for str in report_arr {
-        let findIdx:String.Index = str.firstIndex(of: " ")!
-        reportUser.append(str.substring(to:findIdx))
-        reportedUser.append(str.substring(from:findIdx))
-    }
-    //warning: 'substring(to:)' is deprecated: Please use String slicing subscript with a 'partial range upto' operator.
+  
+    let reportUser = report_arr.map { String($0[..<$0.firstIndex(of:" ")!]) }
+    let reportedUser = report_arr.map { String($0[$0.firstIndex(of:" ")!...]) }
     
     let mappedReportedUser = reportedUser.map { ($0, 1) }
     let counts = Dictionary(mappedReportedUser, uniquingKeysWith: +)
-        
-    var stoppedUser = [String]()
     
-    for i in counts {
-        if i.value >= k {
-            stoppedUser.append(i.key)
-        }
-    }
-        
-    for i in stoppedUser {
-        for (index,str) in reportedUser.enumerated() {
-            if i == str {
-                answer[id_list.firstIndex(of: reportUser[index])!] += 1
+    for (key,value) in counts {
+        if value >= k {
+            for (index,str) in reportedUser.enumerated() {
+                if key == str {
+                    answer[id_list.firstIndex(of: reportUser[index])!] += 1
+                }
             }
         }
     }
