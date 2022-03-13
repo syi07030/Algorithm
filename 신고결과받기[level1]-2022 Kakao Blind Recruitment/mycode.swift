7fbb650c9c2b96b6c7bf21f0c0ae8bc551398a1e
syi07030 <syii07030@gmail.com>
2022-03-13 16:04:40 +0900

    2022.3.13[3]

--- 신고결과받기[level1]-2022 Kakao Blind Recruitment/mycode.swift
+++ 신고결과받기[level1]-2022 Kakao Blind Recruitment/mycode.swift
@@ -8,19 +8,20 @@ func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
     //중복 제거 -> 한 유저가 같은 유저를 여러 번 신고한 경우 신고 횟수는 1번
   
     let reportUser = report_arr.map { String($0[..<$0.firstIndex(of:" ")!]) }
-    let reportedUser = report_arr.map { String($0[$0.firstIndex(of:" ")!...]) }
+    var reportedUser = report_arr.map { String($0[$0.firstIndex(of:" ")!...]) }
+    var new_reportUser = [String]()
     
     let mappedReportedUser = reportedUser.map { ($0, 1) }
     let counts = Dictionary(mappedReportedUser, uniquingKeysWith: +)
     
-    for (key,value) in counts {
-        if value >= k {
-            for (index,str) in reportedUser.enumerated() {
-                if key == str {
-                    answer[id_list.firstIndex(of: reportUser[index])!] += 1
-                }
-            }
+    for (index, str) in reportedUser.enumerated() {
+        if counts[str]! >= k {
+            new_reportUser.append(reportUser[index])
         }
+    }
+    
+    for i in new_reportUser {
+        answer[id_list.firstIndex(of: i)!] += 1
     }
     
     return answer
