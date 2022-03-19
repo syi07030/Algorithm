400ba8928496fb0839aab99dd061487697269bdb
syi07030 <syii07030@gmail.com>
2022-03-19 23:34:18 +0900

    2022.3.19[1]

--- 신규 아이디 추천[level1]-2021 kakao blind recruitment/mycode.swift
+++ 신규 아이디 추천[level1]-2021 kakao blind recruitment/mycode.swift
@@ -5,15 +5,34 @@ func solution(_ new_id:String) -> String {
     var answer = new_id.lowercased()
     
     //2step
-    let pattern = "^[0-9a-z\\-\\_\\.]*$"
+    let pattern = "^[0-9a-z-_.]*$"
+    do {
+        let regex = try NSRegularExpression(pattern: pattern, options: [])
+        let result = regex.matches(in: answer, options: [], range: NSRange(location: 0, length: answer.count))
+    
+        let rexStrings = result.map { (element) -> String in
+            let range = Range(element.range, in: answer)!
+            return String(answer[range])
+        }
+        print(rexStrings)
+    } catch let error {
+        print(error.localizedDescription)
+    }
+    
+    /*
+    let pattern = "^[0-9a-z-_.]*$"
     let regex = try? NSRegularExpression(pattern: pattern)
     
     if let arr = regex?.matches(in: answer, options: [], range: NSRange(location:0,length:answer.count)) {
-        answer = arr.joined(separator: "")
+        let rexString = arr.map { (element) -> String in
+            let range = Range(element.range, in: answer)!
+            return String(answer[range])
+        }
+        print(rexString)
     }else{
         print("error")
     }
-    
+    */
     //3step
     answer = answer.replacingOccurrences(of:".." , with: ".")
     
@@ -27,13 +46,17 @@ func solution(_ new_id:String) -> String {
     
     //6step
     if answer.count >= 16 {
-        answer = String(answer[..<answer.index(answer.startIndex, offsetBy:16)])
+        answer = String(answer[..<answer.index(answer.startIndex, offsetBy:15)])
     }
+    answer=answer.trimmingCharacters(in:["."])
     
     
     //7step
     if answer.count <= 2 {
-        answer = String(repeating: answer, count: 3)
+        let last = String(answer[answer.endIndex])
+        while(answer.count<3){
+            answer += last
+        }
     }
     
     return answer
