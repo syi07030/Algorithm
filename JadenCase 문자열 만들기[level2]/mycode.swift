68071271ea9fddd6905f55ff733c9dd465d9d9a7
syi07030 <syii07030@gmail.com>
2022-03-25 14:22:53 +0900

    2022.3.25[1]

--- JadenCase 문자열 만들기[level2]/mycode.swift
+++ JadenCase 문자열 만들기[level2]/mycode.swift
@@ -1,8 +1,21 @@
+// 뭔가 좀 노가다 느낌이,,
 func solution(_ s:String) -> String {
-    var arr = s.components(separatedBy:" ")
+    var arr = Array(s.lowercased())
+    var answer = [String(arr[0].uppercased())]
+    print(answer)
+    for i in stride(from:1,to:arr.count,by:1){
+        if arr[i-1] == " "{
+            answer += [String(arr[i].uppercased())]
+        }else{
+            answer+=[String(arr[i])]
+        }
+    }
+    return answer.joined(separator:"")
+
+    /*
+    var arr = s.lowercased().components(separatedBy:" ")
     for i in stride(from:0,to:arr.count,by:1){
-        arr[i] = arr[i].lowercased()
         arr[i] = String(arr[i].first!.uppercased())+String(arr[i].dropFirst(1))
     }
-    return arr.joined(separator:" ")
+    return arr.joined(separator:" ")*/
 }
