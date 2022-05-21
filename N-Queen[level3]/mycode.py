38242022ec26b1deefb7a9ef427a9b5336ec450c
syi07030 <syii07030@gmail.com>
2022-05-21 23:57:00 +0900

    2022.5.21[1]

--- N-Queen[level3]/mycode.py
+++ N-Queen[level3]/mycode.py
@@ -7,3 +7,15 @@ def solution(n):
         
     
     return answer
+
+def dfs(i,n,arr): #i-몇 번째 행, n-어떤 열에 놓을지
+    if len(arr) == 0 :
+        dfs()
+        
+def check(y, arr): #퀸을 놓아도 되는지 여부 반환
+    if y in arr: #같은 행에 없도록
+        return False
+    for i in len(arr):
+        if abs(len(arr)-i) == abs(y-arr[i]):
+            return False
+    return True
