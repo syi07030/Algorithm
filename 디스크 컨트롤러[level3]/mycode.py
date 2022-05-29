55770d274ace69c3f849b16c6e3864a3eb615aa7
syi07030 <syii07030@gmail.com>
2022-05-29 10:34:45 +0900

    2022.5.29[1]

--- 디스크 컨트롤러[level3]/mycode.py
+++ 디스크 컨트롤러[level3]/mycode.py
@@ -7,47 +7,30 @@ def solution(jobs):
     #print(jobs)
     current = jobs[0][0] #앞의 task가 끝난 시점, 처음에는 시작 시점으로 초기화
     while len(jobs)>0:
-        if current <= jobs[0][0]:
-            current,same,jobs = sameStart(current,jobs)
-            answer.append(same)
-            print("1",answer,jobs)
+        if jobs[0][0] <= current:
+            current,task,jobs = duringTask(current,jobs)
+            answer.append(task)
+            #print("1",answer,jobs)
         else:
-            current,tasks,jobs = duringTask(current,jobs)
-            print("2",current,tasks,jobs)
-            answer += tasks
-            print("3",answer,jobs)
-    print(sum(answer))
+            current = jobs[0][0]
     return math.trunc(sum(answer)/n)
-
-def sameStart(current,jobs):
-    same = []
-    rest = []
-    time = 0
-    for index,j in enumerate(jobs):
-        if j[0] == current:
-            same.append(j)
-        else:
-            rest = jobs[index:]
-            break
-    result = same[0][1]
-    rest = same[1:]+rest
-    time = current+result
-    return time,result,rest
 
 def duringTask(current, jobs):
     task = []
     rest = []
-    for index,j in enumerate(jobs):
-        if j[0]<current:
-            task.append(j)
-            task.sort(key=lambda x:x[1])
-        else:
-            rest.append(j)
-            break
     time = current
-    result=[time+task[0][1]-task[0][0]]
+    for index,j in enumerate(jobs):
+        if j[0]<=current:
+            task.append(j)
+        else:
+            rest = jobs[index:]
+            break
+    task.sort(key=lambda x:x[1])
+    result=time+task[0][1]-task[0][0]
     time += task[0][1]
-    
-    restSort = task[1:] + rest
-    
-    return  time, result, restSort
+    new_jobs = task[1:] + rest
+
+    return  time, result, new_jobs
+
+
+#다른 사람들 풀이 보면 heapq 모듈 사용 
